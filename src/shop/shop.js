4f762f401e5cb0980f4a97dcc92c984926f6db1e
unknown <charloako1@gmail.com>
2023-08-16 07:18:52 +0300

    added pictures to shop

--- src/shop/shop.js
+++ src/shop/shop.js
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { useState } from 'react';
+import Navbar from '../navbar/Nav';
+import Footer from '../navbar/Footer';
 
+const Shop = () => {
+  const [cart, setCart] = useState([]);
+  
+  const products = [
+    { id: 1,
+     name: 'leash',
+     price: 10,
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNj5v1pkOOKUzrbe5f1NlY0bNJjvujQhiiIA&usqp=CAU' 
+    },
 
-const shop = () => {
-    return(
-        <div>
-            
+    { id: 2,
+    name: 'collar',
+    price: 10,
+    image:'https://i5.walmartimages.com/asr/9ca8f8e9-fcbd-4c84-8197-0fb27f736979.4a53d610b5461f0a5aacff89556cb028.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff' 
+    },
+    { id: 3,
+    name: 'Dog Food',
+    price: 10,
+    image:'https://images-na.ssl-images-amazon.com/images/I/81Z0WaYRYtL._SL1500_.jpg'
+     },
+  { id: 4,
+    name: 'Dog Feeder',
+    price: 10,
+    image:'https://pngimg.com/uploads/dog_food/dog_food_PNG33.png'
+    },
+  ];
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <div>
+    <Navbar/>
+        <div className="bg-gray-100 min-h-screen py-10 py-20 text-center">
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-semibold mb-6">Shop With Us</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <div key={product.id} className="bg-white p-4 shadow-md rounded-md">
+             <div className="aspect-w-4 aspect-h-5 mb-2">
+                <img src={product.image} alt={product.name} className="object-cover object-center w-full h-full" />
+            </div>
+              <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
+              <p className="text-gray-700">${product.price}usd</p>
+              <button
+                onClick={() => addToCart(product)}
+                className="mt-3 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+              >
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
-    )
-}
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-4">Shopping Cart</h2>
+          <ul className="bg-white p-4 shadow-md rounded-md">
+            {cart.map((item, index) => (
+              <li key={index} className="mb-2">
+                {item.name} - ${item.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+  );
+};
 
-export default shop
+export default Shop;
